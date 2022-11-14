b60f87b1664b3d09d228d2e443574d099ac59282
lcourson <3845318+lcourson@users.noreply.github.com>
2022-11-14 13:47:49 -0500

    adding build script

---
+++ makepkg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PLUGIN_NAME="vmsnapshot"
+BASE_DIR="usr/local/emhttp/plugins"
+TMP_DIR="/tmp/${PLUGIN_NAME}_"$(echo $RANDOM)""
+VERSION="$(date +'%Y.%m.%d')"
+SRCDIR=`pwd`
+
+mkdir -p $SRCDIR/packages
+mkdir -p $TMP_DIR/$VERSION/$BASE_DIR
+cd source
+cp --parents -R * $TMP_DIR/$VERSION/$BASE_DIR
+cd $TMP_DIR/$VERSION
+tar -cJf $SRCDIR/packages/$PLUGIN_NAME-$VERSION.txz *
+md5sum $SRCDIR/packages/$PLUGIN_NAME-$VERSION.txz > $SRCDIR/packages/$PLUGIN_NAME-$VERSION.txz.md5
+#rm -R $TMP_DIR/$VERSION/
+cd $SRCDIR
+rm -R $TMP_DIR
